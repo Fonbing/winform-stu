fcf4bb97a930c2f9af0973406f78fd780c8bde81
zjty5418 <zjty5418@163.com>
2016-04-11 10:59:13 +0800

    4-11

---
+++ code/c#/字体循环滚动.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _04winform_loop1
+{
+    public partial class Form1 : Form
+    {
+        int i = 0, t = 1;
+        bool flag = true;
+        Color now;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            richTextBox1.Text = "ABCDEFGHIGKLMNOPQRSTVWXYZ";
+            now = richTextBox1.SelectionColor; //备份当前颜色
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //在一个按钮中实现滚动暂停
+            if (flag)
+            {
+                timer1.Enabled = true;
+                button1.Text = "暂停";
+                flag = false;
+            }
+            else
+            {
+                timer1.Enabled = false;
+                button1.Text = "开始";
+                flag = true;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //返回开头并停止滚动
+            i = 0;
+            t = 1;
+            timer1.Enabled = false;
+            flag = true;
+            button1.Text = "开始";
+            richTextBox1.SelectionColor = now;  //返回初始颜色
+            MessageBoxButtons bt = MessageBoxButtons.YesNo;
+            DialogResult result = MessageBox.Show("是否退出!!!", "提示", bt);
+            if ("Yes" == result.ToString())   //选择是则退出
+                Close();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {  
+            richTextBox1.SelectionColor = now;//返回初始颜色
+            // richTextBox1.SelectionStart = i；
+            richTextBox1.Select(i, 1); //从第i-1个数，开始变颜色，，
+            //richTextBox1.SelectionLength = 1;
+            richTextBox1.SelectionColor = Color.Red;
+            //控制下标实现循环滚动
+            i += t;
+            if (i == 0 || i == richTextBox1.Text.Length - 1)
+                t = -t;
+
+        }
+    }
+}
---
+++ code/c#/数组排序.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SortDemo
+{
+    public partial class Form1 : Form
+    {
+        double[] a = new double[5];
+        int i = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double element = double.Parse(textBox1.Text);
+            a[i] = element;
+            textBox2.Text += a[i] + " ";
+            i++;
+            textBox1.Text = "";
+            label2.Text = "请输入第" + (i + 1) + "个元素";
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Array.Sort(a);
+            for (i = 0; i < 5; i++)
+                textBox3.Text += a[i] + " ";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label2.Text = "请输入第1个元素";
+            this.Text = "排序";
+        }
+    }
+}
